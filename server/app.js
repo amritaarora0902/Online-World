5bbf1a92fcc7ee05c751f24de6eed756315a7e18
amrita <amritaarora2424@gmail.com>
2020-06-18 17:16:27 +0530

    mongoose

---
+++ server/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const PORT = 5000;
+const {MONGOURI} = require('./keys');
+
+
+mongoose.connect(MONGOURI,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+mongoose.connection.on('connected',()=>{
+    console.log("connected to mongo")
+})
+
+mongoose.connection.on('error',(err)=>{
+    console.log("error connecting",err)
+})
+
+
+app.listen(PORT,()=>{
+    console.log("Server is running on",PORT)
+})
