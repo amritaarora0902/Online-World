213fad68df979a3afd016160eda8c39dfda8a472
amrita <amritaarora2424@gmail.com>
2020-06-21 16:46:28 +0530

    home-page-UI

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -4,7 +4,44 @@ import React from 'react';
 
 const Home = () =>{
     return (
-        <h1>home</h1>
+        <div className="home">
+            <div className="card home-card">
+                <h5>Amy</h5>
+                <div className="card-image">
+                    <img src="https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"/>
+                </div>
+                <div className="card-content">
+                <i class="material-icons">favorite</i>
+                    <h6>Title</h6>
+                    <p>this is amazoing post</p>
+                    <input type="text" placeholder="add a comment"/>
+                </div>
+            </div>
+            <div className="card home-card">
+                <h5>Amy</h5>
+                <div className="card-image">
+                    <img src="https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"/>
+                </div>
+                <div className="card-content">
+                <i class="material-icons">favorite</i>
+                    <h6>Title</h6>
+                    <p>this is amazoing post</p>
+                    <input type="text" placeholder="add a comment"/>
+                </div>
+            </div>
+            <div className="card home-card">
+                <h5>Amy</h5>
+                <div className="card-image">
+                    <img src="https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"/>
+                </div>
+                <div className="card-content">
+                <i class="material-icons">favorite</i>
+                    <h6>Title</h6>
+                    <p>this is amazoing post</p>
+                    <input type="text" placeholder="add a comment"/>
+                </div>
+            </div>
+        </div>
     )
 }
 
